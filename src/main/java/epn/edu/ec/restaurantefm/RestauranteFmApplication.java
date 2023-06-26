b1b6c2f1b363ad7e4c15b3529cb7b5a96e6542e5
labSoft1 <luis.pilamunga@epn.edu.ec>
2023-06-25 19:01:24 -0500

    version inicial

---
+++ src/main/java/epn/edu/ec/restaurantefm/RestauranteFmApplication.java
@@ -0,0 +1,13 @@
+package epn.edu.ec.restaurantefm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestauranteFmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestauranteFmApplication.class, args);
+    }
+
+}
